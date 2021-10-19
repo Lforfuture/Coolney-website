19322c28b86ceaaa133facfb483beac2d72acb64
maylin <1078836421@qq.com>
2021-10-20 02:44:56 +0800

    deploy

---
+++ precache-manifest.a066e165cb0492c3cc9161fb9ff27465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c60a29f96bf042e91fd",
+    "url": "/Coolney-website/css/app.80a34959.css"
+  },
+  {
+    "revision": "d99dbfb6279a3cde1e9a",
+    "url": "/Coolney-website/css/chunk-vendors.6b06f038.css"
+  },
+  {
+    "revision": "3c564ce78d8d6d67cacc4a096f5299f4",
+    "url": "/Coolney-website/index.html"
+  },
+  {
+    "revision": "4c60a29f96bf042e91fd",
+    "url": "/Coolney-website/js/app.532a4ca4.js"
+  },
+  {
+    "revision": "d99dbfb6279a3cde1e9a",
+    "url": "/Coolney-website/js/chunk-vendors.6ff819dc.js"
+  },
+  {
+    "revision": "8c703af659a5d81566630553e158785e",
+    "url": "/Coolney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Coolney-website/robots.txt"
+  }
+]);
